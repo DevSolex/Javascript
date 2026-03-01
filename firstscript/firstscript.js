25c3338939ae7172c9037d81baaef0fc4ce9e02e
DevSolex <devsolex6@gmail.com>
2026-03-01 19:10:29 +0100

    initial commit

---
+++ firstscript/firstscript.js
@@ -0,0 +1,7 @@
+console.log ("Hello, World! This is my first JavaScript code.");
+var name = "Solex";
+console.log("My name is " + name);
+
+const my_name = "Solomon"
+
+console.log("my real name is " + my_name)
